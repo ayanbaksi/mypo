9018b384f1900f2afc3b0650e225491f2f24a48b
Ayan Baksi <ayanbaksi2002@gmail.com>
2023-08-17 18:54:06 +0530

    newfix2

--- src/component/CarouselDefault.jsx
+++
@@ -1,8 +0,0 @@
-import Lottie from "react-lottie";
-import groovyWalkAnimation from "../hi.json";
-
-const Example = () => {
-  return <Lottie animationData={groovyWalkAnimation} />;
-};
-
-export default Example;
